8e79321542fc567a63d96b4e28e9f5a4614118ae
Deblina Nandy <73984076+deblinanandy@users.noreply.github.com>
2023-10-24 22:19:13 +0530

    ww

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import router from './router/router.js';
-import Database from './dbconnect.js'
+import Database from './dbconnect.js';
+
 const app = express();
 const port = 8000;
 
-// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-//app.use('/public', express.static('public'));
-
-// Use the Database middleware (assuming it's a valid middleware)
 Database();
-app.use(express.static(path.join(__dirname, './client/bulid')));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
-// Routes
-app.use('/ap1', router);
 
-// Start the server
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.use('/api', router);
+
 app.listen(port, () => {
   console.log('Connected to port ' + port);
 });
