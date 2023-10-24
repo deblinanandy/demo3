025c1eb5ba55c2d36adf731940349cd401018ff8
Deblina Nandy <73984076+deblinanandy@users.noreply.github.com>
2023-10-24 22:23:51 +0530

    a

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
 import path from 'path';
 import router from './router/router.js';
 import Database from './dbconnect.js';
+
+const __filename = fileURLToPath(import.meta.url); // Use import.meta.url to get the current module's URL
+const __dirname = path.dirname(__filename); // Use path.dirname to get the directory name
 
 const app = express();
 const port = 8000;
